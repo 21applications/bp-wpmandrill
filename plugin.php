0b77c7f9ebe818ef4a82e25b6a7d936d40c713f9
Roger Coathup <roger@21applications.com>
2016-04-12 14:56:15 +0100

    Update plugin.php
    Added licence reference

--- plugin.php
+++ plugin.php
@@ -6,6 +6,23 @@ Description: Allows the new BuddyPress HTML email to be sent using Mandrill.
 Author: Roger Coathup
 Version: 1.0
 Author URI: http://21applications.com
+License: GPLv2
+*/
+
+/*
+This program is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details. 
+
+You should have received a copy of the GNU General Public License 
+along with this program; if not, write to the Free Software 
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+
 */
 
 // Exit if accessed directly
